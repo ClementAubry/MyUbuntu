1c6c6a1fb878fbf79cad8d04a6cfbe9edb5ad8d0
ClementAubry <clement.aubry@gmail.com>
2017-03-11 16:30:30 +0100

    Modification postinstall for 16.04.1LTS

--- UbuntuPostInstall/ubuntu-16.04LTS-postinstall.py
+++ UbuntuPostInstall/ubuntu-16.04LTS-postinstall.py
@@ -10,13 +10,13 @@
 # Distributed under the GPL version 3 license
 #
 
-__appname__ = 'ubuntu-16.04-postinstall'
+__appname__ = 'ubuntu-16.04.1-postinstall'
 __version__ = "0.1"
 __author__ = "Clement Aubry from Nicolas Hennion <nicolas@nicolargo.com>"
 __licence__ = "LGPL"
 
 """
-Post installation script for Ubuntu 16.04
+Post installation script for Ubuntu 16.04.1
 """
 
 import os
@@ -36,7 +36,7 @@ gettext.install(__appname__)
 _FOR_UBUNTU = "xenial"
 _DEBUG = 1
 _LOG_FILE = "/tmp/%s.log" % __appname__
-_CONF_FILE = "https://raw.githubusercontent.com/ClementAubry/MyUbuntu/master/UbuntuPostInstall/ubuntu-16.04LTS-postinstall-turtlebotROSKinetic.cfg"
+_CONF_FILE = "https://raw.githubusercontent.com/ClementAubry/MyUbuntu/master/UbuntuPostInstall/ubuntu-16.04.1LTS-postinstall.cfg"
 
 # System commands
 #-----------------------------------------------------------------------------
@@ -93,7 +93,7 @@ def syntax():
     """
     Print the script syntax
     """
-    print(_("Ubuntu 14.04 post installation script version %s for %s")
+    print(_("Ubuntu 16.04.1 post installation script version %s for %s")
                                             % (__version__, _FOR_UBUNTU))
     print("")
     print(_("Syntax: %s.py [-c cfgfile] [-h] [-v]") % __appname__)
@@ -390,7 +390,7 @@ def main(argv):
 
     showexec (_("chmod on what have been done here"), "chown -R $SUDO_USER:$SUDO_USER $HOME")
     showexec (_("Add user to vbousers to be able to use usb ports"), "usermod -G vboxusers -a $SUDO_USER")
-    showexec (_("Add user to dialout group"), "addgroup $USERNAME dialout")
+    showexec (_("Add user to dialout group"), "addgroup $SUDO_USER dialout")
 
     # End of the script
     print("---")
